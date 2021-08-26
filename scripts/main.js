ca85f31b8504bb5860561574306d7ce0b3dbc598
kxvxnc <potatokevin123@gmail.com>
2021-08-26 11:18:35 -0400

    UI Changes

--- scripts/main.js
+++ scripts/main.js
@@ -4,13 +4,15 @@ async function getLanyard() {
     .then(
         function(data) {
             if (data.data.spotify) {
+                document.getElementById("listening").innerHTML = "Listening To:";
                 document.getElementById("cover").src = data.data.spotify.album_art_url;
                 document.getElementById("song_title").innerHTML = data.data.spotify.song;
                 document.getElementById("artist").innerHTML = data.data.spotify.artist;
                 document.getElementById("song_link").href = "https://open.spotify.com/track/" + data.data.spotify.track_id;
             } else {
+                document.getElementById("listening").innerHTML = "Not listening to anything right now!";
                 document.getElementById("cover").src = "/assets/song.jpeg";
-                document.getElementById("song_title").innerHTML = "I'm not listening to anything right now!";
+                document.getElementById("song_title").innerHTML = "";
                 document.getElementById("artist").innerHTML = "";
                 document.getElementById("song_link").href = "https://open.spotify.com";
             }
